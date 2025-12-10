b9618e3e35b84a78318f7ecda0ce7a8573872edb
dvazarc110 <dvazarc110@g.educaand.es>
2025-12-10 09:11:34 +0100

    Refactorizacion del programa y optimizacion del codigo (Version B)

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Factorial {
 
+	private static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Introduce un número entero mayor o igual que 0: ");
-        int num = sc.nextInt();
+        int num = pedirNumero();
 
         if (num < 0) {
             System.out.println("ERROR: el número debe ser mayor o igual que 0.");
@@ -17,6 +17,11 @@ public class Factorial {
         }
 
         sc.close();
+	}
+	
+	private static int pedirNumero() {
+	    System.out.print("Introduce un número entero mayor o igual que 0: ");
+	    return sc.nextInt();
 	}
 
 }
